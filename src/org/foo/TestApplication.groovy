f007d1e22a65a055830c2368486339888cd250d0
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-29 13:19:30 +0200

    Update TestApplication.groovy

--- src/org/foo/TestApplication.groovy
+++ src/org/foo/TestApplication.groovy
@@ -5,15 +5,14 @@ def execute() {
             echo 'beginnning workflow...'
           
             stage 'prepare gems'
-            withEnv(['PATH=/usr/bin:/bin:/usr/sbin:/:sbin/usr/local/bin:$PATH']) {
-                sh '''#!/bin/bash
-                gem update bundler
-                gem --version
-                gem update --system
-                bundle install --path=.bundle/gems/
-                '''
-            }
-          
+            sh '''#!/bin/bash
+            which bundle
+            gem update bundler
+            gem --version
+            gem update --system
+            bundle install --path=.bundle/gems/
+            '''
+                      
             stage 'rspec testing'
             sh '''#!/bin/bash
             bundle exec rake test
