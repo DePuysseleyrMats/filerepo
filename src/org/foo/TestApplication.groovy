7cab2368bc81f2d78086e1c0bf7b31021d4437c2
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-30 08:34:50 +0200

    Update TestApplication.groovy

--- src/org/foo/TestApplication.groovy
+++ src/org/foo/TestApplication.groovy
@@ -6,7 +6,7 @@ def execute() {
     
             stage 'prepare gems'
             sh '''#!/bin/bash
-            sudo source /home/matsdp/.rvm/scripts/rvm
+            source /home/matsdp/.rvm/scripts/rvm
             rvm use 2.1.5
             echo $PATH
             ruby --version
