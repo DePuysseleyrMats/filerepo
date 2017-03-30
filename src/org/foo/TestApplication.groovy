06c1f27973f74844a97046305e4386254adecb22
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-03-30 13:11:49 +0200

    Update TestApplication.groovy

--- src/org/foo/TestApplication.groovy
+++ src/org/foo/TestApplication.groovy
@@ -8,13 +8,12 @@ def execute() {
             sh '''#!/bin/bash
             source /usr/local/rvm/scripts/rvm
             rvm use 2.1.5
-            echo $PATH
-            ruby --version
             bundle -v
             gem update --system
             gem update bundler
             gem --version
             gem update --system
+            pwd
             bundle install --path=vendor/bundle
             '''
                       
