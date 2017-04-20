857115e8074d4ae0fc75a8ec38d345d7578b4e43
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-04-20 14:55:32 +0200

    Update TestApplication.groovy

--- src/org/foo/TestApplication.groovy
+++ src/org/foo/TestApplication.groovy
@@ -3,7 +3,7 @@ package org.foo;
 def execute() {
   
   
-  node {
+  node('testslave') {
             docker.image('testslave').inside {
 
               wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
