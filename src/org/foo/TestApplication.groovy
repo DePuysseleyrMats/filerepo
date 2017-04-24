abdbe46a5d3dcde828959f11437303f6fc53d91a
DePuysseleyrMats <mats.depuysseleyr.u9654@student.hogent.be>
2017-04-24 08:44:17 +0200

    Update TestApplication.groovy

--- src/org/foo/TestApplication.groovy
+++ src/org/foo/TestApplication.groovy
@@ -3,38 +3,41 @@ package org.foo;
 def execute() {
   
   
-  node('testslave') {
+  node {
             
-            withDockerContainer('testslave').inside {
-
+            
+              stage 'Prepare environment'
               wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
-              checkout scm
-              echo 'beginnning workflow...'       
+              def environmnent = docker.build 'testslave'  
+                environment.inside {
+                  checkout scm
+                  echo 'beginnning workflow...'       
 
-              stage 'prepare gems'
-              sh '''#!/bin/bash
-              source /usr/local/rvm/scripts/rvm
-              rvm use 2.1.5
-              bundle -v
-              gem update --system
-              gem update bundler
-              gem --version
-              gem update --system
-              bundle install --path=vendor/bundle
-              '''
+                  stage 'prepare gems'
+                  sh '''#!/bin/bash
+                  source /usr/local/rvm/scripts/rvm
+                  rvm use 2.1.5
+                  bundle -v
+                  gem update --system
+                  gem update bundler
+                  gem --version
+                  gem update --system
+                  bundle install --path=vendor/bundle
+                  '''
 
-              stage 'rspec testing'
-              sh '''#!/bin/bash
-              source /usr/local/rvm/scripts/rvm
-              bundle exec rake test SPEC_OPTS="--format documentation" 
-              '''
+                  stage 'rspec testing'
+                  sh '''#!/bin/bash
+                  source /usr/local/rvm/scripts/rvm
+                  bundle exec rake test SPEC_OPTS="--format documentation" 
+                  '''
 
-              stage 'beaker testing'
-              sh '''#!/bin/bash
-              source /usr/local/rvm/scripts/rvm
-              VBoxManage --version
-              bundle exec rake beaker SPEC_OPTS="--format documentation" 
-              '''  
+                  stage 'beaker testing'
+                  sh '''#!/bin/bash
+                  source /usr/local/rvm/scripts/rvm
+                  VBoxManage --version
+                  bundle exec rake beaker SPEC_OPTS="--format documentation" 
+                  '''  
+                 }
               }
             }
           }
